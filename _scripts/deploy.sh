91f9c6c269226093eb268a8f903fa1deaec13601
hermetico <yojmarinv@gmail.com>
2017-03-03 19:50:17 +0100

    more tests

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -56,9 +56,8 @@ ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../_scripts/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
-#eval `ssh-agent -s`
-#ssh-add deploy_key
-mv deploy_key ~/.ssh/id_rsa
+eval `ssh-agent -s`
+ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
