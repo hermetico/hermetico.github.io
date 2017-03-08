50f8261a4560e506af8009496e2a0b09165642f5
hermetico <yojmarinv@gmail.com>
2017-03-08 16:02:11 +0100

    yep, deploying, error at install.sh

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,7 @@
 set -e # Exit with nonzero exit code if anything fails
 
 # our branches
-SOURCE_BRANCH="master"
+SOURCE_BRANCH="devel"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
